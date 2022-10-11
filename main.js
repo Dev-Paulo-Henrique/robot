efb7598d7148de1caadeb602332cb670d754c7de
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2022-10-11 06:49:28 -0300

    Add math

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ const walking = 10;
 const jumpAndRun = 11;
 const wave = 12;
 const sayYes = 13;
+
+var msg = new SpeechSynthesisUtterance();
+
 var output = document.querySelector("#output").textContent;
 
 var audio = new Audio(
@@ -33,6 +36,9 @@ var areYouthere = 0;
         if (event.results[i].isFinal) {
           const content = event.results[i][0].transcript.trim();
           output = content;
+          // var mds = [content.split(" ")]
+          // alert(output)
+          // console.log(content.split(" "))
         }
       }
     };
@@ -55,12 +61,26 @@ var camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 2;
 camera.position.y = 1;
+scene.background = new THREE.Color(0xefefff);
+scene.fog = new THREE.Fog(0xefefff, 10, 50);
 var light = new THREE.DirectionalLight(0xefefff, 3);
 light.position.set(1, 1, 1).normalize();
 scene.add(light);
-var light = new THREE.DirectionalLight(0xffefef, 3);
-light.position.set(-1, -1, -1).normalize();
-scene.add(light);
+const hemiLight = new THREE.HemisphereLight(0xefefff, 3);
+hemiLight.position.set(-1, -1, -1).normalize();
+scene.add(hemiLight);
+//Divisão do fundo
+const mesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(2000, 2000),
+  new THREE.MeshPhongMaterial({ color: 0x87ceeb, depthWrite: false })
+);
+mesh.rotation.x = -Math.PI / 2;
+scene.add(mesh);
+//Quadriculado
+const grid = new THREE.GridHelper(200, 40, 0x000000, 0x000000);
+grid.material.opacity = 0.2;
+grid.material.transparent = true;
+scene.add(grid);
 window.addEventListener("resize", function () {
   let width = window.innerWidth;
   let height = window.innerHeight;
@@ -84,10 +104,7 @@ loader.load("RobotExpressive.glb", function (gltf) {
   model.scale.set(0.35, 0.35, 0.35);
   scene.add(model);
 
-  
   mixer = new THREE.AnimationMixer(model);
-  //   let playLooking =
-  //   let stopLooking =
 
   mixer.clipAction(gltf.animations[wave]).fadeIn(0.5).setLoop(THREE.LoopOnce);
   mixer.clipAction(gltf.animations[wave]).play();
@@ -97,24 +114,56 @@ loader.load("RobotExpressive.glb", function (gltf) {
       audio.play();
       output = "";
     }
-    if (
-      output === "Oi" ||
-      output === "olá" ||
-      output === "tchau"
-    ) {
+    if (output === "Oi" || output === "olá" || output === "tchau") {
       mixer.clipAction(gltf.animations[wave]).fadeIn(0.5).stop();
       mixer.clipAction(gltf.animations[wave]).fadeIn(0.5).play();
       output = "";
     }
-    if (output === "corre") {
-      mixer.clipAction(gltf.animations[look]).fadeOut(0.5).play();
+    if (output === "senta" || output === "sente") {
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
+      mixer.clipAction(gltf.animations[sitting]).fadeIn(1).play();
+      setTimeout(() => {
+        mixer.clipAction(gltf.animations[sitting]).setEffectiveTimeScale(0);
+      }, 200);
+      output = "";
+    }
+    if (output === "levanta" || output === "levante") {
+      mixer.clipAction(gltf.animations[sitting]).fadeOut(1).play();
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
+      // mixer.clipAction(gltf.animations[standing]).fadeIn(0.5).play();
+      setTimeout(() => {
+        mixer.clipAction(gltf.animations[sitting]).setEffectiveTimeScale(1);
+        mixer.clipAction(gltf.animations[sitting]).stop();
+        mixer.clipAction(gltf.animations[look]).fadeIn(0.5).play();
+        // mixer.clipAction(gltf.animations[standing]).setEffectiveTimeScale(0);
+      }, 1000);
+      output = "";
+    }
+    if (output === "dança" || output === "dance") {
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
+      mixer.clipAction(gltf.animations[dance]).fadeIn(0.5).play();
+      setTimeout(() => {
+        mixer.clipAction(gltf.animations[look]).stop();
+      }, 500);
+      output = "";
+    }
+    if (output === "pare de dançar" || output === "para de dançar") {
+      mixer.clipAction(gltf.animations[dance]).fadeOut(0.5).play();
+      setTimeout(() => {
+        mixer.clipAction(gltf.animations[dance]).stop();
+        mixer.clipAction(gltf.animations[look]).fadeIn(0.5).play();
+      }, 500);
+      output = "";
+    }
+    if (output === "corre" || output === "corra") {
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
       mixer.clipAction(gltf.animations[run]).fadeIn(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[look]).stop();
       }, 500);
       output = "";
     }
-    if (output === "pare de correr") {
+    if (output === "pare de correr" || output === "para de correr") {
       mixer.clipAction(gltf.animations[run]).fadeOut(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[run]).stop();
@@ -122,15 +171,15 @@ loader.load("RobotExpressive.glb", function (gltf) {
       }, 500);
       output = "";
     }
-    if (output === "anda") {
-      mixer.clipAction(gltf.animations[look]).fadeOut(0.5).play();
+    if (output === "anda" || output === "ande") {
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
       mixer.clipAction(gltf.animations[walking]).fadeIn(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[look]).stop();
       }, 500);
       output = "";
     }
-    if (output === "pare de andar") {
+    if (output === "pare de andar" || output === "para de andar") {
       mixer.clipAction(gltf.animations[walking]).fadeOut(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[walking]).stop();
@@ -138,8 +187,18 @@ loader.load("RobotExpressive.glb", function (gltf) {
       }, 500);
       output = "";
     }
+    if (output === "quer me bater") {
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
+      mixer.clipAction(gltf.animations[punch]).play();
+      setTimeout(() => {
+        mixer.clipAction(gltf.animations[punch]).fadeOut(0.5).play();
+        mixer.clipAction(gltf.animations[look]).stop();
+        mixer.clipAction(gltf.animations[look]).fadeIn(0.5).play();
+      }, 400);
+      output = "";
+    }
     if (output === "tudo bem com você") {
-      mixer.clipAction(gltf.animations[look]).fadeOut(0.5).play();
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
       mixer.clipAction(gltf.animations[sayYes]).fadeIn(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[sayYes]).fadeOut(0.5).play();
@@ -148,18 +207,20 @@ loader.load("RobotExpressive.glb", function (gltf) {
       }, 1500);
       output = "";
     }
-    if (output === "salta" || output === "pula") {
-      mixer.clipAction(gltf.animations[look]).fadeOut(0.5).play();
+    if (output === "salta" || output === "pule") {
+      mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
       mixer.clipAction(gltf.animations[jump]).fadeIn(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[jump]).fadeOut(0.5).play();
-        mixer.clipAction(gltf.animations[jump]).stop();
         mixer.clipAction(gltf.animations[look]).stop();
         mixer.clipAction(gltf.animations[look]).fadeIn(0.5).play();
-      }, 600);
+      }, 500);
+      setTimeout(() => {
+        mixer.clipAction(gltf.animations[jump]).stop();
+      }, 700);
       output = "";
     }
-    if (output === "finge de morto") {
+    if (output === "finge de morto" || output === "desliga") {
       mixer.clipAction(gltf.animations[look]).fadeIn(0.5).stop();
       mixer.clipAction(gltf.animations[dead]).fadeIn(0.5).play();
       setTimeout(() => {
@@ -167,7 +228,7 @@ loader.load("RobotExpressive.glb", function (gltf) {
       }, 900);
       output = "";
     }
-    if (output === "volta aqui") {
+    if (output === "volta aqui" || output === "ressuscita") {
       mixer.clipAction(gltf.animations[dead]).fadeOut(0.5).play();
       setTimeout(() => {
         mixer.clipAction(gltf.animations[dead]).setEffectiveTimeScale(1);
@@ -195,7 +256,44 @@ loader.load("RobotExpressive.glb", function (gltf) {
       }, 1600);
       output = "";
     }
-  }, 1000);
+    if (output.split(" ")[0] === "soma" || output.split(" ")[0] === "somar") {
+      msg.text = `O resultado da soma é: ${
+        parseInt(output.split(" ")[1]) + parseInt(output.split(" ")[3])
+      }`;
+      window.speechSynthesis.speak(msg);
+      output = "";
+    }
+    if (
+      output.split(" ")[0] === "subtrai" ||
+      output.split(" ")[0] === "subtrair"
+    ) {
+      msg.text = `O resultado da subtração é: ${
+        parseInt(output.split(" ")[1]) - parseInt(output.split(" ")[3])
+      }`;
+      window.speechSynthesis.speak(msg);
+      output = "";
+    }
+    if (
+      output.split(" ")[0] === "multiplica" ||
+      output.split(" ")[0] === "multiplicar"
+    ) {
+      msg.text = `O resultado da multiplicação é: ${
+        parseInt(output.split(" ")[1]) * parseInt(output.split(" ")[3])
+      }`;
+      window.speechSynthesis.speak(msg);
+      output = "";
+    }
+    if (
+      output.split(" ")[0] === "divida" ||
+      output.split(" ")[0] === "dividir"
+    ) {
+      msg.text = `O resultado da divisão é: ${
+        parseInt(output.split(" ")[1]) / parseInt(output.split(" ")[3])
+      }`;
+      window.speechSynthesis.speak(msg);
+      output = "";
+    }
+  }, 100);
 });
 
 var clock = new THREE.Clock();
@@ -203,7 +301,8 @@ function render() {
   requestAnimationFrame(render);
   var delta = clock.getDelta();
   if (mixer != null) mixer.update(delta);
-  //if (model) model.rotation.y += 0.025;
+  // if (model) camera.rotation.y += 0.025;
+  // if (model) model.rotation.y += 0.025;
 
   renderer.render(scene, camera);
 }
